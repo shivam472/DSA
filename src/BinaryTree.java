5f5c97ba8dfae8c181745a2062f9a3a8c604716f
Shivam Pandey <shivamraj472@gmail.com>
2022-03-05 15:47:30 +0530

    height, size, max, min

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -25,6 +25,12 @@ public class BinaryTree {
         System.out.println();
         System.out.println("PostOrder traversal: ");
         bt.postOrder(root);
+        System.out.println();
+        System.out.println("Height of the tree: " + bt.height(root));
+        System.out.println("Maximum: " + bt.maximum(root));
+        System.out.println("Minimum: " + bt.minimum(root));
+        bt.levelOrder(root);
+        System.out.println();
     }
 
     public Node createTree() {
@@ -68,6 +74,57 @@ public class BinaryTree {
         postOrder(root.left);
         postOrder(root.right);
         System.out.print(root.data + " ");
+    }
+
+    // function to print each level
+    private void printCurrentLevel(Node root, int level) {
+        if(root == null) return;
+
+        if(level == 1) {
+            System.out.print(root.data + " ");
+        }
+        else {
+            printCurrentLevel(root.left, level-1);
+            printCurrentLevel(root.right, level-1);
+        }
+    }
+
+
+    // time complexity for this approach -> O(n^2)
+    public void levelOrder(Node root) {
+        if(root == null) return;
+
+        for(int i = 0;  i <= height(root); i++) {
+            printCurrentLevel(root, i);
+        }
+    }
+
+    // height of a binary tree
+    public int height(Node root) {
+        if(root == null) return 0;
+
+        return Math.max(height(root.left), height(root.right)) + 1;
+    }
+
+    // size of a binary tree -> total number of nodes
+    public int size(Node root) {
+        if(root == null) return 0;
+        
+        return size(root.left) + size(root.right) + 1;
+    }
+
+    // maximum in a binary tree
+    public int maximum(Node root) {
+        if(root == null) return Integer.MIN_VALUE;
+
+        return Math.max(root.data, Math.max(maximum(root.left), maximum(root.right)));
+    }
+
+    // minimum in a binary tree
+    public int minimum(Node root) {
+        if(root == null) return Integer.MAX_VALUE;
+
+        return Math.min(root.data, Math.min(minimum(root.left), minimum(root.right)));
     }
     
 }
