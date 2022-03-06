02061445d239505b42746d5db7efdbc011f88970
Shivam Pandey <shivamraj472@gmail.com>
2022-03-06 17:01:44 +0530

    QuickSort - noob implementation

---
+++ src/Algorithms/src/sorting/QuickSortRunner.java
@@ -0,0 +1,48 @@
+package Algorithms.src.sorting;
+
+public class QuickSortRunner {
+
+    public static int partition(int[] a, int l, int h) {
+        int pivot = a[l];
+        int i = l;
+        int j = h;
+
+        while(i < j) {
+            while(a[i] <= pivot) i++;
+            while(a[j] > pivot) j--;
+
+            if(i < j) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        // swap jth and lth element
+        int temp = a[j];
+        a[j] = a[l];
+        a[l] = temp;
+
+        return j; // partitioning position
+    }
+
+    // time complexity (best case) -> O(nlogn)
+    // time complexity (worst case) -> O(n^2) -> if the array is already sorted
+    public static void QuickSort(int a[], int l, int h) {
+        if(l < h) {
+            int j = partition(a, l, h); // returns the partitioning position
+            QuickSort(a, l, j-1);
+            QuickSort(a, j+1, h);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 16, 8, 12, 15, 6, 3, 9, 5};
+        QuickSort(arr, 0, 8);
+
+        System.out.println("Sorted array: ");
+        for(int x : arr) {
+            System.out.print(x + " ");
+        }
+
+    }
+}
