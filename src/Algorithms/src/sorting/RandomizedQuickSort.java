b5004b42ba85e9f9f0c5f2a2150a614e430cb5b3
Shivam Pandey <shivamraj472@gmail.com>
2022-03-06 17:59:30 +0530

    RandomizedQuickSort - optimised quick sort O(nlogn) complexity

---
+++ src/Algorithms/src/sorting/RandomizedQuickSort.java
@@ -0,0 +1,76 @@
+package Algorithms.src.sorting;
+
+import java.util.Random;
+
+public class RandomizedQuickSort {
+
+    // Fisher–Yates shuffle
+    public static void shuffle(int[] a) {
+        int index, temp;
+        Random r = new Random();
+
+        // Start from the last element and swap one by one. We don't
+        // need to run for the first element that's why i > 0
+        for(int i = a.length-1; i > 0; i--) {
+            // Pick a random index from 0 to i
+            index = r.nextInt(i+1);
+
+            // if index = i, no need to swap
+            if(index != i) {
+                // Swap arr[i] with the element at random index
+                temp = a[index];
+                a[index] = a[i];
+                a[i] = temp;
+            }
+        }
+    }
+
+    public static int partition(int[] a, int l, int h) {
+        int pivot = a[l];
+        int i = l;
+        int j = h;
+
+        while(i < j) {
+            while(a[i] <= pivot) i++;
+            while(a[j] > pivot) j--;
+
+            if(i < j) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+        // swap jth and lth element
+        int temp = a[j];
+        a[j] = a[l];
+        a[l] = temp;
+
+        return j; // partitioning position
+    }
+
+    // time complexity (worst case) -> O(nlogn)
+    // l -> low
+    // h -> high
+    public static void QuickSort(int a[], int l, int h) {
+        if(l < h) {
+            int j = partition(a, l, h); // returns the partitioning position
+            QuickSort(a, l, j);
+            QuickSort(a, j+1, h);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 16, 8, 12, 15, 6, 3, 9, 5};
+        
+        // To randomly shuffle the array to achieve best case complexity
+        shuffle(arr);
+
+        QuickSort(arr, 0, arr.length-1);
+
+        System.out.println("Sorted array: ");
+        for(int x : arr) {
+            System.out.print(x + " ");
+        }
+
+    }
+}
